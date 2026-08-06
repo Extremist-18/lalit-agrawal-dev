79c167c48b28f1f30136d7615fedcce6c9a70ccf
Extremist-18 <137435109+Extremist-18@users.noreply.github.com>
2026-08-06 12:48:30 +0530

    updates

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,8 +15,8 @@ const About = ()=>{
                             I enjoy designing systems that are fast, reliable, and efficient. Whether it's reducing latency, improving throughput, optimizing infrastructure costs, or simplifying user workflows, I'm constantly looking for ways to make software perform better and save users valuable time.
 
                         <ul className="points">
-                            <li><a href="https://codeforces.com/profile/Lalit_Agrawal" target="_blank" className="anchor">CodeChef</a>: Specialist (Max:1583) &amp; <a href="https://atcoder.jp/users/Lalit18" target="_blank" className="anchor">AtCoder</a>: 4Kyu (Max:1232) </li>
-                            <li> <a href="https://www.codechef.com/users/lalit_agrawal" target="_blank" className="anchor">CodeChef</a>: 3 Star (Max:1719) &amp; <a href="https://leetcode.com/u/Lalit__Agrawal/" target="_blank" className="anchor">Leetcode</a>: Knight (Max:1966) </li>
+                            <li><a href="https://codeforces.com/profile/Lalit_Agrawal" target="_blank" className="anchor">CodeChef</a>: Expert (Max:1661) &amp; <a href="https://atcoder.jp/users/Lalit18" target="_blank" className="anchor">AtCoder</a>: 4Kyu (Max:1232) </li>
+                            <li> <a href="https://www.codechef.com/users/lalit_agrawal" target="_blank" className="anchor">CodeChef</a>: 4 Star (Max:1800) &amp; <a href="https://leetcode.com/u/Lalit__Agrawal/" target="_blank" className="anchor">Leetcode</a>: Knight (Max:1968) </li>
                             <li>AIR 24 in CodeSmash 4.0 organized by Mercer|Mettl among 1.2 Million participants</li>
                         </ul>
                         </p>
